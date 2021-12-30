1f6a3eda5795ccebbbb6798196f59d8d993cbb36
Sherub Thakur <sherub.thakur@gmail.com>
2021-12-30 11:52:28 +0530

    Starting point to write implementation

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,29 +1,5 @@
 use log::debug;
-use std::{convert::TryFrom, net::UdpSocket};
 use structopt::StructOpt;
-
-use rdns::{write_packet, DnsHeader, DnsPacket, DnsQuestion, QueryType, MAX_PACKET_SIZE};
-
-static RESOLVER_SERVER: (&str, u16) = ("8.8.8.8", 53);
-static UDP_RESPONSE_LISTENER: (&str, u16) = ("0.0.0.0", 2053);
-
-fn mk_query(qname: &str, qtype: QueryType) -> DnsPacket {
-    let header = DnsHeader::builder()
-        .id(818) // This should be a random number
-        .questions(1)
-        .recursion_desired(true)
-        .build();
-
-    let question = DnsQuestion {
-        name: qname.to_string(),
-        qtype,
-    };
-
-    DnsPacket::builder()
-        .header(header)
-        .questions(vec![question])
-        .build()
-}
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "DNS Client", about = "Search DNS records for a given query")]
@@ -39,39 +15,7 @@ fn main() -> anyhow::Result<()> {
     let Opt { query } = StructOpt::from_args();
     debug!("args {}", query);
 
-    let socket = UdpSocket::bind(UDP_RESPONSE_LISTENER)?;
-
-    let request = mk_query(&query, QueryType::A);
-    debug!("Request: {:#?}", request);
-
-    let mut req_buffer = vec![0u8; MAX_PACKET_SIZE];
-    let size = write_packet(&mut req_buffer, &request)?;
-
-    socket.send_to(&req_buffer[..size], RESOLVER_SERVER)?;
-
-    let mut response_buffer = vec![0u8; MAX_PACKET_SIZE];
-    let (size, _src) = socket.recv_from(&mut response_buffer)?;
-
-    let response = DnsPacket::try_from(&response_buffer[..size]).map_err(anyhow::Error::msg)?;
-
-    print_packet(&response);
+    // TODO: Write DNS client
 
     Ok(())
-}
-
-pub fn print_packet(packet: &DnsPacket) {
-    println!("{:#?}", packet.header);
-
-    for q in &packet.questions {
-        println!("{:#?}", q);
-    }
-    for rec in &packet.answers {
-        println!("{:#?}", rec);
-    }
-    for rec in &packet.authorities {
-        println!("{:#?}", rec);
-    }
-    for rec in &packet.resources {
-        println!("{:#?}", rec);
-    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use std::{convert::TryFrom, net::UdpSocket};
+use std::net::UdpSocket;
 
-use log::{debug, info};
-use rdns::{resolve, write_packet, DnsPacket, MAX_PACKET_SIZE};
+use log::info;
 
 static DNS_SERVER: (&str, u16) = ("127.0.0.1", 2053);
 
@@ -9,20 +8,9 @@ fn main() -> anyhow::Result<()> {
     env_logger::init();
 
     info!("Starting DNS Server: {:?}", DNS_SERVER);
-    let socket = UdpSocket::bind(DNS_SERVER)?;
+    let _socket = UdpSocket::bind(DNS_SERVER)?;
 
-    loop {
-        let mut request_buffer = vec![0u8; MAX_PACKET_SIZE];
-        let (size, source) = socket.recv_from(&mut request_buffer)?;
+    // TODO: Write the DNS server
 
-        let request = DnsPacket::try_from(&request_buffer[..size]).map_err(anyhow::Error::msg)?;
-        debug!("Request: {:?}, from: {:?}", request, source);
-
-        let response = resolve(request)?;
-        debug!("Response: {:?}", response);
-
-        let mut response_buffer = vec![0u8; MAX_PACKET_SIZE];
-        let size = write_packet(&mut response_buffer, &response)?;
-        socket.send_to(&response_buffer[..size], source)?;
-    }
+    Ok(())
 }
